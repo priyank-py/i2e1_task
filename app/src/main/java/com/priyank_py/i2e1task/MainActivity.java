a79fb08b2d0a2f9077fc7af47b423dfb55584088
Priyank <ipriyank007@hotmail.com>
2020-06-08 13:45:27 +0530

    first commit

---
+++ app/src/main/java/com/priyank_py/i2e1task/MainActivity.java
@@ -0,0 +1,14 @@
+package com.priyank_py.i2e1task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
